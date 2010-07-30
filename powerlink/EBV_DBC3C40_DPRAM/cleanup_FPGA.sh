b373034c5be8977223336ff2aed6f944f4d9753f
hoggerm <hoggerm@0f5f4594-6328-11df-85c8-e91c4881b994>
2010-07-30 12:16:27 +0000

    Dual Nios design runs with PresChaining.
    ---------------------
    Detailed Description:
    ---------------------
    
    HW-Changes:
    -----------------
    Added sof and jdi file to version control (for instant running of design without HW compilation).
    
    SW-Changes:
    -----------------
    - some minor build-file adaptions
    
    Status:
    -----------------
    Dual-Nios Design runs, also with PresChaining. SimpleIO might not work with new stack version, because it doesn't use openMAC 1.1 currently
    
    Compliance:
    -----------------
    This Version is compliant to the openPOWERLINK 1.6.0.6 with openMAC 1.1.
    --- See B&R Internal openPOWERLINK SVN Rev. 55 ---
    
    ------------------------------------------------- 
    TODO:
    --------
    - Adapt triple buffer: 2 Ack registers for each PDO needed; Multiple PDO capability needed.
    - Adapt SW: Multiple PDO capability needed.
    - Find a better place for cleanup_FPGA.sh
    - Adapt SimpleIO for MAC 1.1
    
    -------------------------------------------------
    KNOWN ISSUES:
    --------------------
    - AS does not import XDDs with PresChaining correctly
    - When programming the FPGA with "nios2-configure-sof" there is some bug:
      If the 7-Seg should state "FF", all LEDs below should be on plus 1 LED next to the Phy.
      The 24V LEDs should be turned off.
     => If you see something else after "nios2-configure-sof" do one power-cycle and reprogram again, until its correct.
    
    
    
    
    
    git-svn-id: http://ategge1109/svn/openPOWERLINK_CNDK/trunk@19 0f5f4594-6328-11df-85c8-e91c4881b994

--- powerlink/EBV_DBC3C40_DPRAM/cleanup_FPGA.sh
+++ powerlink/EBV_DBC3C40_DPRAM/cleanup_FPGA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###########################################################################################
-# This script creates the openPOWERLINK Digital I/O Interface application in this directory.
+# This script cleans the FPGA Quartus II design. It deletes the generated files.
 ###########################################################################################
 
 ###############################################################################
@@ -8,16 +8,9 @@
 #
 #  TODO: Copy this file to the PCP directory (powerlink/DPRAM)
 #
-#
+################################################################################
  
 FPGADESIGN=../../fpga/altera/EBV_DBC3C40/nios2_openmac_dpram_multinios
-
-# cp ${FPGADESIGN}/*.bdf		${FPGADESIGN}/EPL 		# top level schematic
-# cp ${FPGADESIGN}/*.ptf		${FPGADESIGN}/EPL		# SOPC Builder system description for generation (peripheral template file)
-# cp ${FPGADESIGN}/*.sopc 		${FPGADESIGN}/EPL		# SOPC Builder design file
-# cp ${FPGADESIGN}/*.sopcinfo 	${FPGADESIGN}/EPL		# SOPC Builder report file
-# cp ${FPGADESIGN}/*.qpf		${FPGADESIGN}/EPL		# Quartus project file
-# cp ${FPGADESIGN}/*.qsf		${FPGADESIGN}/EPL		# Quartus settings file
 
 # User added files
 
@@ -61,6 +54,8 @@ cp ${FPGADESIGN}/lpm_ff0.vhd							${FPGADESIGN}/EPL
 cp ${FPGADESIGN}/lpm_inv0.vhd                           ${FPGADESIGN}/EPL
 cp ${FPGADESIGN}/niosII_openMac.vhd                     ${FPGADESIGN}/EPL
 cp ${FPGADESIGN}/sevsegEncode.vhd                       ${FPGADESIGN}/EPL
+cp ${FPGADESIGN}/nios_openMac.jdi                       ${FPGADESIGN}/EPL # *.jdi is a generated file. Needed for elf-programming.
+cp ${FPGADESIGN}/nios_openMac.sof						${FPGADESIGN}/EPL # *.sof is a generated file. Its left though because of instant run design possibility.
                                                       
 													  
 mv ${FPGADESIGN}/EPL ./ 							# save FPGA desing
@@ -70,16 +65,9 @@ rm -r ${FPGADESIGN}/db
 rm -r ${FPGADESIGN}/.sopc_builder
 rm -r ${FPGADESIGN}/incremental_db
 rm -r ${FPGADESIGN}/incremental_dbniosII_openMac_sim
+rm -r ${FPGADESIGN}/niosII_openMac_sim
 
 mv ./EPL ${FPGADESIGN}								# restore basic files
-
-
-# mv ${FPGADESIGN}/EPL/*.bdf		${FPGADESIGN}		# move desing files to right place
-# mv ${FPGADESIGN}/EPL/*.ptf		${FPGADESIGN}
-# mv ${FPGADESIGN}/EPL/*.sopc 	${FPGADESIGN}
-# mv ${FPGADESIGN}/EPL/*.sopcinfo ${FPGADESIGN}
-# mv ${FPGADESIGN}/EPL/*.qpf		${FPGADESIGN}
-# mv ${FPGADESIGN}/EPL/*.qsf		${FPGADESIGN}
 
 mv ${FPGADESIGN}/EPL/niosII_openMac_generation_script	  ${FPGADESIGN}    
 mv ${FPGADESIGN}/EPL/digital_io.bdf                       ${FPGADESIGN}    
@@ -120,7 +108,9 @@ mv ${FPGADESIGN}/EPL/lpm_constant0.vhd                    ${FPGADESIGN}
 mv ${FPGADESIGN}/EPL/lpm_ff0.vhd                          ${FPGADESIGN}    
 mv ${FPGADESIGN}/EPL/lpm_inv0.vhd                         ${FPGADESIGN}    
 mv ${FPGADESIGN}/EPL/niosII_openMac.vhd                   ${FPGADESIGN}    
-mv ${FPGADESIGN}/EPL/sevsegEncode.vhd                     ${FPGADESIGN}    
+mv ${FPGADESIGN}/EPL/sevsegEncode.vhd                     ${FPGADESIGN}
+mv ${FPGADESIGN}/EPL/nios_openMac.jdi                     ${FPGADESIGN}
+mv ${FPGADESIGN}/EPL/nios_openMac.sof                   ${FPGADESIGN}     
                                                       
 	echo " "                                         
 	echo " "                                          
