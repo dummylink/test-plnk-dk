a7a5913947d055f8ab75b7b1973e897c1312256a
hoggerm <hoggerm@0f5f4594-6328-11df-85c8-e91c4881b994>
2010-12-10 18:12:54 +0000

    Sof file was missing
    ---------------------
    Detailed Description:
    ---------------------
    
    HW-Changes:
    ------------
    POWERLINK-IP updated to Rev. 42
    
    SW-Changes:
    -----------------
    
    
    Status:
    -----------------
    * successfully tested with XP Demo
    * successfully tested with B&R MN 800us cycle time
    
    Compliance:
    -----------------
    - openPOWERLINK Stack V1.6.0.7 or V1.7
    - Win XP MN demo Rev. 29.
    
    ------------------------------------------------- 
    TODO:
    --------
    - SDO transfer
    - Object write & read 
    
    -------------------------------------------------
    KNOWN ISSUES:
    --------------------
    - Bugs as described in lower revision
    - SPI with SyncIR can not do low cycle times on AP side -> use POLLING MODE 
    
    
    git-svn-id: http://ategge1109/svn/openPOWERLINK_CNDK/trunk@64 0f5f4594-6328-11df-85c8-e91c4881b994

--- libCnApi/include/cnApiCfg.h
+++ libCnApi/include/cnApiCfg.h
@@ -3,7 +3,7 @@ It is automatically build according to PCP system.h, every time
 a rebuild of the PCP software is executed. */
 
 /* Defines for CN API Library */
-#define CN_API_USING_SPI
-#define AP_IS_LITTLE_ENDIAN
+//INVALID_VALUE
+//INVALID_VALUE
 #define POWERLINK_0_PDI_PCP_PDIRPDOS 3
 #define POWERLINK_0_PDI_PCP_PDITPDOS 1
