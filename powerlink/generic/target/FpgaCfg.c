d43e6ea1550636b780c03528f9de9055da98a225
Michael Hogger <michael.hogger@br-automation.com>
2011-07-20 08:30:24 +0200

    minor: comment

--- powerlink/generic/target/FpgaCfg.c
+++ powerlink/generic/target/FpgaCfg.c
@@ -341,7 +341,7 @@ tFpgaCfgRetVal FpgaCfg_handleReconfig(void)
                  IORD_ALTERA_AVALON_SYSID_TIMESTAMP(SYSID_BASE),
                  IORD_ALTERA_AVALON_SYSID_ID(SYSID_BASE));
 
-    /* verify if BSP matches SOPC build time stamp */
+    /* verify if BSP matches SOPC system ID */
     if (alt_avalon_sysid_test() != 0)
     {
         DEBUG_TRACE0(DEBUG_LVL_ALWAYS, "Bad image! FPGA configuration does not match SW!");
