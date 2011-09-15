21e499ae848d715dbf71120229243300d223c896
Josef Baumgartner <josef.baumgartner@br.automation.com>
2011-09-15 13:58:42 +0200

    working changes

--- powerlink/generic/target/altera/update.c
+++ powerlink/generic/target/altera/update.c
@@ -16,14 +16,61 @@ This module contains firmware update functions.
 
 /******************************************************************************/
 /* includes */
+#include "system.h"
 #include "firmware.h"
+#include <sys/alt_flash.h>
+#include <alt_types.h>
+
+/******************************************************************************/
+/* defines */
+#define UPDATE_STATE_NONE       0
+#define UPDATE_STATE_START      1
+#define UPDATE_STATE_FPGA       2
+#define UPDATE_STATE_PCP        3
+#define UPDATE_STATE_AP         4
+#define UPDATE_STATE_IIB        5
+
+
+#define USER_IMAGE_OFFSET       0x000D0000
+
+/* check if a flag in a variable is set */
+#define FLAG_ISSET(VAR,FLAG)          ((VAR & FLAG) == FLAG)
 
 /******************************************************************************/
 /* type definitions */
+typedef enum {
+   eUpdateResultAbort = 0x0001,
+   eUpdateResultPending = 0x0002,
+   eUpdateResultSegFinish = 0x0004,
+   eUpdateResultPartFinish = 0x0008,
+   eUpdateResultCRCErr = 0x0010,
+   eUpdateResultEraseFinish = 0x0020
+} tUpdateResult;
+
+typedef enum {
+    eUpdateStateNone = 0,
+    eUpdateStateStart,
+    eUpdateStateFpga,
+    eUpdateStatePcp,
+    eUpdateStateAp,
+    eUpdateStateIib
+} tUpdateState;
+
 typedef struct {
-    UINT32              m_deviceId;
-    UINT32              m_hwRev;
-    UINT32              m_offset;
+    UINT32              m_uiDeviceId;
+    UINT32              m_uiHwRev;
+    UINT32              m_uiUserImageOffset
+    UINT32              m_uiSectorSize
+    char *              m_pData;
+    UINT32              m_uiDataSize;
+    UINT32              m_uiEraseOffset;
+    UINT32              m_uiProgOffset;
+    UINT32              m_uiRemainingSize;
+    UINT32              m_uiCrc;
+    void *              m_pfnAbortCb;
+    void *              m_pfnSegFinishCb;
+    UINT32              m_uiUpdateState;
+    alt_flash_fd *      m_flashFd;
 } tUpdateInfo;
 
 /******************************************************************************/
@@ -48,7 +95,8 @@ to the global variable fwHeader_g for further reference.
 getFwHeader(tFwHeader *pHeader_p, UINT32 deviceId_p, UINT32 hwRev_p)
 {
     /* check header CRC */
-    if (crc32(0, pData_p, sizeof(tFwHeader) - sizeof(UINT32)) != pHeader_p->m_headerCrc)
+    if (crc32(0, pData_p, sizeof(tFwHeader) - sizeof(UINT32)) !=
+              pHeader_p->m_headerCrc)
     {
         /* wrong CRC */
         return ERROR;
@@ -63,7 +111,8 @@ getFwHeader(tFwHeader *pHeader_p, UINT32 deviceId_p, UINT32 hwRev_p)
     }
 
     /* check device ID and hardware revision */
-    if ((pHeader_p->m_deviceId != deviceId_p) || (pHeader_p->m_hwRevision != hwRev_p))
+    if ((pHeader_p->m_deviceId != deviceId_p) ||
+        (pHeader_p->m_hwRevision != hwRev_p))
     {
         /* firmware for another device or hardware revision */
         return ERROR;
@@ -97,9 +146,9 @@ initFirmwareUpdate() initializes all things needed for a firmware update.
 *******************************************************************************/
 int initFirmwareUpdate(UINT32 deviceId_p, UINT32 hwRev_p)
 {
-    updateInfo_g.m_deviceId = deviceId_p;
-    updateInfo_g.m_hwRev = hwRev_p;
-    updateInfo_g.m_userImageOffset = uiUserImageOffset_p;
+    updateInfo_g.m_uiDeviceId = deviceId_p;
+    updateInfo_g.m_uiHwRev = hwRev_p;
+    updateInfo_g.m_uiUserImageOffset = USER_IMAGE_OFFSET;
 }
 
 /**
@@ -114,43 +163,334 @@ SDO object handler for object 0x1F50.
 \return		return
 \retval		return_value			return_value_description
 *******************************************************************************/
-int updateFirmware(UINT32 * pSegmentOff_p, UINT32 * pSegmentSize_p, char * pData_p)
+int updateFirmware(UINT32 * pSegmentOff_p, UINT32 * pSegmentSize_p, char * pData_p,
+        void *pfnAbortCb_p, void * pfnSegFinishCb_p)
 {
+    int                 iRet;
+    flash_region*       aFlashRegions;  ///< flash regions array
+    unsigned short      wNumOfRegions;  ///< number of flash regions
+
     /* The first segment of the SDO transfer starts with the firmware header */
     if (*pSegmentOff_p == 0)
     {
-        updateInfo_g.m_progOffset = 0;
+        updateInfo_g.m_uiProgOffset = 0;
 
         /* get firmware header and check if we receive a valid one */
         if (getFwHeader(pData_p,
-                        updateInfo_g.m_deviceId,
-                        updateInfo_g.m_hwRev) == ERROR)
+                        updateInfo_g.m_uiDeviceId,
+                        updateInfo_g.m_uiHwRev) == ERROR)
         {
             return ERROR;
         }
 
-        remainingSize = fwHeader_g.m_fpgaConfigSize + fwHeader_g.m_pcpSwSize;
-        offset += sizeof(fwHeader);
+        /* open flash device */
+        if ((updateInfo_g.m_flashFd =
+                alt_flash_open_dev(EPCS_FLASH_CONTROLLER_0_NAME)) == NULL)
+        {
+            return ERROR;
+        }
+        /* get some flash information */
+        if ((iRet = alt_get_flash_info(updateInfo_g.m_flashFd,
+                                       &aFlashRegions,
+                                       (int*) &wNumOfRegions)) != 0)
+        {
+            return ERROR;
+        }
+        updateInfo_g.m_uiSectorSize = aFlashRegions->block_size;
 
-        size = *pSegmentSize_p - sizeof(fwHeader);
+        updateInfo_g.m_pData = pData_p + sizeof(tFwHeader);
+        updateInfo_g.m_uiDataSize = *pSegmentSize_p - sizeof(tFwHeader);
+        updateInfo_g.m_uiUpdateState = UPDATE_STATE_START;
     }
     else
     {
-        size = *pSegmentSize_p;
+        /* check if last segment is not yet processed */
+        if (updateInfo_g.m_uiDataSize != 0)
+        {
+            return ERROR;
+        }
+        else
+        {
+            updateInfo_g.m_pData = pData_p;
+            updateInfo_g.m_uiDataSize = *pSegmentSize_p;
+        }
     }
 
-    if (updateI)
-
-    if (programming)
-    {
-
-    progSize = (size <= remainingSize) ? size : remainingSize;
-    if (programFirmware(pData_p, progSize, offset) < 0)
-    {
-        return ERROR;
-    }
-
-
-    /* TODO: detect last segment to do cleanups! */
-
+    updateInfo_g.m_pfnAbortCb = pfnAbortCb_p;
+    updateInfo_g.m_pfnSegFinishCb = pfnSegFinishCb_p;
 }
+
+/**
+********************************************************************************
+\brief  start state
+*******************************************************************************/
+void updateStateStart(void)
+{
+    int iRet;
+
+    iRet = eraseIib();
+
+    if (FLAG_ISSET(iRet, eUpdateResultAbort))
+    {
+        updateInfo_g.m_pfnAbortCb();
+        updateInfo_g.m_uiUpdateState = UPDATE_STATE_NONE;
+    }
+
+    if (FLAG_ISSET(iRet, eUpdateResultEraseFinish))
+    {
+        updateInfo_g.m_uiProgOffset = updateInfo_g.m_uiUserImageOffset;
+        /* FPGA configuration must always start on a sector boundary */
+        updateInfo_g.m_uiEraseOffset = updateInfo_g.m_uiProgOffset;
+        updateInfo_g.m_uiRemainingSize = fwHeader_g.m_fpgaConfigSize;
+        updateInfo_g.m_uiCrc = 0;
+        updateInfo_g.m_uiUpdateState = UPDATE_STATE_FPGA;
+    }
+}
+
+/**
+********************************************************************************
+\brief  fpga update state
+*******************************************************************************/
+void updateStateFpga(void)
+{
+    int         iRet;
+
+    iRet = programFirmware();
+
+    if (FLAG_ISSET(iRet, eUpdateResultAbort))
+    {
+        updateInfo_g.m_pfnAbortCb();
+        updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+        return;
+    }
+
+    if (FLAG_ISSET(iRet, eUpdateResultPartFinish))
+    {
+        /* FPGA part of image finished, check CRC */
+        if (updateInfo_g.m_uiCrc == fwHeader_g.m_fpgaConfigCrc)
+        {
+            /* CRC is right, we could continue with the PCP software */
+            updateInfo_g.m_uiProgOffset = updateInfo_g.m_uiUserImageOffset
+                                          + fwHeader_g.m_fpgaConfigSize;
+            /* roundup erase offset to next sector start */
+            updateInfo_g.m_uiEraseOffset = (updateInfo_g.m_uiProgOffset +
+                    updateInfo_g.m_uiSectorSize - 1) &
+                    (updateInfo_g.m_uiSectorSize - 1);
+            updateInfo_g.m_uiRemainingSize = fwHeader_g.m_pcpSwSize;
+            updateInfo_g.m_uiCrc = 0;
+            updateInfo_g.m_uiUpdateState = eUpdateStatePcp;
+
+            /* notify SDO stack if segment is finished */
+            if (FLAG_ISSET(iRet, eUpdateResultSegFinish))
+            {
+                updateInfo_g.m_pfnSegFinishCb();
+            }
+        }
+        else
+        {
+            /* abort due to wrong CRC */
+            updateInfo_g.m_pfnAbortCb();
+            updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+        }
+    }
+    else
+    {
+        /* notify SDO stack that segment is finished */
+        if (FLAG_ISSET(iRet, eUpdateResultSegFinish))
+        {
+            updateInfo_g.m_pfnSegFinishCb();
+        }
+    }
+}
+
+/**
+********************************************************************************
+\brief  pcp software update state
+*******************************************************************************/
+void updateStatePcp(void)
+{
+    int         iRet;
+
+    iRet = programFirmware();
+
+    if (FLAG_ISSET(iRet, eUpdateResultAbort))
+    {
+        updateInfo_g.m_pfnAbortCb();
+        updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+        return;
+    }
+
+    /* check if the PCP part is finished */
+    if (FLAG_ISSET(iRet, eUpdateResultPartFinish))
+    {
+        /* PCP software part of image finished, check CRC */
+        if (updateInfo_g.m_uiCrc == fwHeader_g.m_pcpSwCrc)
+        {
+            /* CRC is right, we could continue with the AP software */
+
+            /* notify SDO stack if segment is finished */
+            if (FLAG_ISSET(iRet, eUpdateResultSegFinish))
+            {
+                updateInfo_g.m_pfnSegFinishCb();
+            }
+
+            /* check if AP software is contained in update */
+            if (fwHeader_g.m_apSwSize == 0)
+            {
+                /* no ap software, continue with setting up IIB */
+                updateInfo_g.m_uiUpdateState = eUpdateStateIib;
+            }
+            else
+            {
+                updateInfo_g.m_uiProgOffset = updateInfo_g.m_uiUserImageOffset +
+                                              fwHeader_g.m_fpgaConfigSize +
+                                              fwHeader_g.m_pcpSwSize;
+                /* roundup erase offset to next sector start */
+                updateInfo_g.m_uiEraseOffset = (updateInfo_g.m_uiProgOffset +
+                        updateInfo_g.m_uiSectorSize - 1) &
+                        (updateInfo_g.m_uiSectorSize - 1);
+                updateInfo_g.m_uiRemainingSize = fwHeader_g.m_apSwSize;
+                updateInfo_g.m_uiCrc = 0;
+                updateInfo_g.m_uiUpdateState = eUpdateStateAp;
+            }
+        }
+        else
+        {
+            /* abort due to wrong CRC */
+            updateInfo_g.m_pfnAbortCb();
+            updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+        }
+    }
+    else
+    {
+        /* notify SDO stack that segment is finished */
+        if (FLAG_ISSET(iRet, eUpdateResultSegFinish))
+        {
+            updateInfo_g.m_pfnSegFinishCb();
+        }
+    }
+}
+
+/**
+********************************************************************************
+\brief  AP software update state
+*******************************************************************************/
+void updateStateAp(void)
+{
+    int         iRet;
+
+    iRet = programFirmware();
+
+    if (FLAG_ISSET(iRet, eUpdateResultAbort))
+    {
+        updateInfo_g.m_pfnAbortCb();
+        updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+        return;
+    }
+
+    /* check if the AP part is finished */
+    if (FLAG_ISSET(iRet, eUpdateResultPartFinish))
+    {
+        /* AP software part of image finished, check CRC */
+        if (updateInfo_g.m_uiCrc == fwHeader_g.m_apSwCrc)
+        {
+            /* CRC is right, we could continue with the IIB */
+            updateInfo_g.m_uiUpdateState = eUpdateStateIib;
+
+            /* notifiy SDO stack if segment is finished
+             * NOTE: should always be the case because it
+             * is the last part of the update!
+             */
+            if (FLAG_ISSET(iRet, eUpdateResultSegFinish))
+            {
+                updateInfo_g.m_pfnSegFinishCb();
+            }
+            else
+            {
+                /* we are finished with the AP part but there is
+                 * remaining data! Abort the update! */
+                updateInfo_g.m_pfnAbortCb();
+                updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+            }
+        }
+        else
+        {
+            /* abort due to wrong CRC */
+            updateInfo_g.m_pfnAbortCb();
+            updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+        }
+    }
+    else
+    {
+        /* notify SDO stack that segment is finished */
+        if (FLAG_ISSET(iRet, eUpdateResultSegFinish))
+        {
+            updateInfo_g.m_pfnSegFinishCb();
+        }
+    }
+}
+
+/**
+********************************************************************************
+\brief  IIB setup state
+*******************************************************************************/
+void updateStateIib(void)
+{
+    int         iRet;
+    tIib        iib;
+
+    /* setup IIB */
+    iib.m_magic = IIB_MAGIC_V2;
+    iib.m_applicationSwDate = fwHeader_g.m_applicationSwDate;
+    iib.m_applicationSwTime = fwHeader_g.m_applicationSwTime;
+    iib.m_fpgaConfigCrc = fwHeader_g.m_fpgaConfigCrc;
+    iib.m_fpgaConfigSize = fwHeader_g.m_fpgaConfigSize;
+    iib.m_pcpSwCrc = fwHeader_g.m_pcpSwCrc;
+    iib.m_pcpSwSize = fwHeader_g.m_pcpSwSize;
+    iib.m_apSwCrc = fwHeader_g.m_apSwCrc;
+    iib.m_apSwSize = fwHeader_g.m_apSwSize;
+    memset(iib.m_reserved, 0, sizeof (iib.m_reserved));
+
+    /* calculate CRC of IIB */
+    crc32 (0, &iib, sizeof(iib) - sizeof(UINT32));
+
+    /* program IIB into flash */
+    br_epcs_flash_write_block(updateInfo_g.m_flashFd,
+                              block_offset,
+                              data_offset,
+                              &iib, sizeof(iib));
+
+    updateInfo_g.m_uiUpdateState = eUpdateStateNone;
+}
+
+
+/**
+********************************************************************************
+\brief  periodic worker function for firmware update
+*******************************************************************************/
+int updateFirmwarePeriodic(void)
+{
+    switch (updateInfo_g.m_uiUpdateState)
+    {
+    case eUpdateStateStart:
+        updateStateStart();
+        break;
+
+    case eUpdateStateFpga:
+        updateStateFpga();
+        break;
+
+    case eUpdateStatePcp:
+        updateStatePcp();
+        break;
+
+    case eUpdateStateAp:
+        updateStateAp();
+        break;
+
+    case eUpdateStateIib:
+        updateStateIib();
+        break;
+    }
+}
+
--- powerlink/include/firmware.h
+++ powerlink/include/firmware.h
@@ -23,6 +23,8 @@ This header file contains definitions for the firmware update functions.
 #define         FW_HEADER_MAGIC         0x4657          ///< magic "FW"
 #define         FW_HEADER_VERSION       0x0100          ///< version 1.0
 
+#define         IIB_MAGIC_V2            0x49494202      ///< IIB version 2
+
 /******************************************************************************/
 /* type definitions */
 
@@ -48,5 +50,30 @@ typedef struct {
     UINT32      m_reserved;             ///< reserved
     UINT32      m_headerCrc;            ///< CRC32 checksum of firmware header
 } tFwHeader;
+
+
+/**
+ * tIib defines the image information block
+ */
+typedef struct {
+    UINT32      m_magic;                ///< magic number and version "IIBx"
+    UINT32      m_applicationSwDate;    ///< application software date
+    UINT32      m_applicationSwTime;    ///< application software time
+    UINT32      m_fpgaConfigVersion;    ///< version of fpga configuration
+    UINT32      m_fpgaConfigAdrs;       ///< address of fpga configuration
+    UINT32      m_fpgaConfigSize;       ///< size of fpga configuration
+    UINT32      m_fpgaConfigCrc;        ///< CRC32 checksum of fpga configuration
+    UINT32      m_pcpSwVersion;         ///< version of PCP software
+    UINT32      m_pcpSwAdrs;            ///< address of PCP software
+    UINT32      m_pcpSwSize;            ///< size of PCP software
+    UINT32      m_pcpSwCrc;             ///< CRC32 checkusm of PCP software
+    UINT32      m_apSwVersion;          ///< version of AP software
+    UINT32      m_apSwAdrs;             ///< address of AP software
+    UINT32      m_apSwSize;             ///< size of AP software
+    UINT32      m_apSwCrc;              ///< CRC32 checksum of AP software
+    UINT8       m_reserved[64];         ///< reserved for future use
+    UINT32      m_iibCrc;               ///< checksum of IIB
+} tIib;
+
 
 #endif /* FIRMWARE_H_ */
