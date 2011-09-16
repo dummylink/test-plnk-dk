9866b5d5d25dc8520a9e63ab24e5102ca5db626b
Josef Baumgartner <josef.baumgartner@br.automation.com>
2011-09-16 15:15:14 +0200

    Add modified epcs HAL driver

---
+++ fpga/altera/TERASIC_DE2-115/ink_ap_pcp_intavalon/altera_avalon_epcs_flash_controller/HAL/inc/epcs_commands_ext.h
@@ -0,0 +1,28 @@
+/* extended epcs commands file, based on altera epcs HAL driver */
+
+
+#include "alt_types.h"
+
+#define epcs_read    0x03
+#define epcs_pp      0x02
+#define epcs_wren    0x06
+#define epcs_wrdi    0x04
+#define epcs_rdsr    0x05
+#define epcs_wrsr    0x01
+#define epcs_se      0xD8
+#define epcs_be      0xC7
+#define epcs_dp      0xB9
+#define epcs_res     0xAB
+#define epcs_rdid    0x9F
+
+alt_u8 epcs_read_device_id_ext(alt_u32 base);
+alt_u8 epcs_read_electronic_signature_ext(alt_u32 base);
+alt_u8 epcs_read_status_register_ext(alt_u32 base);
+int epcs_sector_erase_ext(alt_u32 base, alt_u32 offset);
+alt_32 epcs_read_buffer_ext(alt_u32 base, int offset, alt_u8 *dest_addr, int length);
+void epcs_write_enable_ext(alt_u32 base);
+void epcs_write_status_register_ext(alt_u32 base, alt_u8 value);
+alt_32 epcs_write_buffer_ext(alt_u32 base, int offset, const alt_u8 *src_addr, int length);
+
+
+
---
+++ fpga/altera/TERASIC_DE2-115/ink_ap_pcp_intavalon/altera_avalon_epcs_flash_controller/HAL/src/epcs_commands_ext.c
@@ -0,0 +1,281 @@
+/******************************************************************************
+* epcs_commands_ext.c
+*
+* (C) BERNECKER + RAINER, AUSTRIA, A-5142 EGGELSBERG, B&R STRASSE 1
+*
+* This is a modified altera EPCS flash driver. It is changed to immediately
+* return when a flash erase cycle is started. This is needed if application
+* doesn't allow to block for the long time the erase cycle needs. As long
+* as the erase cycle is not completed br_epcs_flash_erase_block() returns
+* -EAGAIN. If another sector erase is started while the previous one is in
+* progress br_epcs_flash_erase_block() returns -EIO.
+******************************************************************************/
+
+/******************************************************************************
+*                                                                             *
+* License Agreement                                                           *
+*                                                                             *
+* Copyright (c) 2008 Altera Corporation, San Jose, California, USA.           *
+* All rights reserved.                                                        *
+*                                                                             *
+* Permission is hereby granted, free of charge, to any person obtaining a     *
+* copy of this software and associated documentation files (the "Software"),  *
+* to deal in the Software without restriction, including without limitation   *
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,    *
+* and/or sell copies of the Software, and to permit persons to whom the       *
+* Software is furnished to do so, subject to the following conditions:        *
+*                                                                             *
+* The above copyright notice and this permission notice shall be included in  *
+* all copies or substantial portions of the Software.                         *
+*                                                                             *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  *
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    *
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE *
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      *
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     *
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         *
+* DEALINGS IN THE SOFTWARE.                                                   *
+*                                                                             *
+* This agreement shall be governed in all respects by the laws of the State   *
+* of California and by the laws of the United States of America.              *
+*                                                                             *
+******************************************************************************/
+
+#include <errno.h>
+#include "alt_types.h"
+#include "epcs_commands_ext.h"
+#include "altera_avalon_spi.h"
+
+alt_u8 epcs_read_status_register_ext(alt_u32 base)
+{
+  const alt_u8 rdsr = epcs_rdsr;
+  alt_u8 status;
+  alt_avalon_spi_command(
+    base,
+    0,
+    1,
+    &rdsr,
+    1,
+    &status,
+    0
+  );
+
+  return status;
+}
+
+static ALT_INLINE int epcs_test_wip(alt_u32 base)
+{
+  return epcs_read_status_register_ext(base) & 1;
+}
+
+static ALT_INLINE int epcs_await_wip_released(alt_u32 base)
+{
+#if 0
+  /* Wait until the WIP bit goes low. */
+  while (epcs_test_wip(base))
+  {
+  }
+#else
+  if (epcs_test_wip(base))
+      return -EAGAIN;
+#endif
+}
+
+int epcs_sector_erase_ext(alt_u32 base, alt_u32 offset)
+{
+  alt_u8 se[4];
+  static int eraseInProgress = 0;
+  static alt_u32 savedBase;
+  static alt_u32 savedOffset;
+
+  
+  if (!eraseInProgress)
+  {
+      se[0] = epcs_se;
+      se[1] = (offset >> 16) & 0xFF;
+      se[2] = (offset >> 8) & 0xFF;
+      se[3] = offset & 0xFF;
+
+      alt_avalon_spi_command(
+        base,
+        0,
+        sizeof(se) / sizeof(*se),
+        se,
+        0,
+        (alt_u8*)0,
+        0
+      );
+      savedBase = base;
+      savedOffset = offset;
+  }
+  else
+  {
+      /* don't accept erase commands for other sectors until
+       * current erase is ready!
+       */
+      if ((base != savedBase) || (offset != savedOffset))
+      {
+          return -EIO;
+      }
+  }
+
+  if (epcs_await_wip_released(base) == -EAGAIN)
+  {
+      eraseInProgress = 1;
+      return -EAGAIN;
+  }
+  else
+  {
+      eraseInProgress = 0;
+      return 0;
+  }
+}
+
+alt_32 epcs_read_buffer_ext(alt_u32 base, int offset, alt_u8 *dest_addr, int length)
+{
+  alt_u8 read_command[4];
+  
+  read_command[0] = epcs_read;
+  read_command[1] = (offset >> 16) & 0xFF;
+  read_command[2] = (offset >> 8) & 0xFF;
+  read_command[3] = offset & 0xFF;
+
+#if 0
+  /* If a write is in progress, fail. */
+  if (epcs_test_wip(base))
+    return 0;
+#endif
+  /* I don't know why this is necessary, since I call await-wip after
+   * all writing commands.
+  */
+  epcs_await_wip_released(base);
+
+  alt_avalon_spi_command(
+    base,
+    0,
+    sizeof(read_command) / sizeof(*read_command),
+    read_command,
+    length,
+    (alt_u8*)dest_addr,
+    0
+  );
+
+  return length;
+}
+
+void epcs_write_enable_ext(alt_u32 base)
+{
+  const alt_u8 wren = epcs_wren;
+  alt_avalon_spi_command(
+    base,
+    0,
+    1,
+    &wren,
+    0,
+    (alt_u8*)0,
+    0
+  );
+}
+
+void epcs_write_status_register_ext(alt_u32 base, alt_u8 value)
+{
+  alt_u8 wrsr[2];
+  
+  wrsr[0] = epcs_wrsr;
+  wrsr[1] = value;
+
+  alt_avalon_spi_command(
+    base,
+    0,
+    2,
+    wrsr,
+    0,
+    (alt_u8*)0,
+    0
+  );
+
+  epcs_await_wip_released(base);
+}
+
+/* Write a partial or full page, assuming that page has been erased */
+alt_32 epcs_write_buffer_ext(alt_u32 base, int offset, const alt_u8* src_addr, int length)
+{
+  alt_u8 pp[4];
+  
+  pp[0] = epcs_pp;
+  pp[1] = (offset >> 16) & 0xFF;
+  pp[2] = (offset >> 8) & 0xFF;
+  pp[3] = offset & 0xFF;
+
+  /* First, WREN */
+  epcs_write_enable_ext(base);
+
+  /* Send the PP command */
+  alt_avalon_spi_command(
+    base,
+    0,
+    sizeof(pp) / sizeof(*pp),
+    pp,
+    0,
+    (alt_u8*)0,
+    ALT_AVALON_SPI_COMMAND_MERGE
+  );
+
+  /* Send the user's buffer */
+  alt_avalon_spi_command(
+    base,
+    0,
+    length,
+    src_addr,
+    0,
+    (alt_u8*)0,
+    0
+  );
+
+  /* Wait until the write is done.  This could be optimized -
+   * if the user's going to go off and ignore the flash for
+   * a while, its writes could occur in parallel with user code
+   * execution.  Unfortunately, I have to guard all reads/writes
+   * with wip-tests, to make that happen.
+   */
+  epcs_await_wip_released(base);
+
+  return length;
+}
+
+
+alt_u8 epcs_read_electronic_signature_ext(alt_u32 base)
+{
+  const alt_u8 res_cmd[] = {epcs_res, 0, 0, 0};
+  alt_u8 res;
+
+  alt_avalon_spi_command(
+    base,
+    0,
+    sizeof(res_cmd) / sizeof(*res_cmd),
+    res_cmd,
+    1,
+    &res,
+    0
+  );
+
+  return res;
+}
+
+alt_u8 epcs_read_device_id_ext(alt_u32 base)
+{
+  const alt_u8 rd_id_cmd[] = {epcs_rdid, 0, 0};
+  alt_u8 res;
+
+  alt_avalon_spi_command(
+    base,
+    0,
+    sizeof(rd_id_cmd) / sizeof(*rd_id_cmd),
+    rd_id_cmd,
+    1,
+    &res,
+    0
+  );
+
+  return res;
+}
