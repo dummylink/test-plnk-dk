1425e6ab024e8edbe21d4b0649734dba4ee7d2df
Michael Hogger <michael.hogger@br-automation.com>
2011-10-13 16:54:49 +0200

    Reduced number of possible SDO connections to 1 because SDO forwarding to AP allows only 1 connection

--- powerlink/pcp_PDI/EplCfg.h
+++ powerlink/pcp_PDI/EplCfg.h
@@ -240,8 +240,8 @@
 
 // do not increase the number of SDO channels, because we are not master
 //#define EPL_SDO_MAX_CONNECTION_ASND 100
-//#define EPL_MAX_SDO_SEQ_CON         100
-//#define EPL_MAX_SDO_COM_CON         100
+#define EPL_MAX_SDO_SEQ_CON         1
+#define EPL_MAX_SDO_COM_CON         1
 //#define EPL_SDO_MAX_CONNECTION_UDP  50
 
 
