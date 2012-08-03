f4e36ff76c01e33011b4fd1058390ffd0c920bb6
Michael Hogger <michael.hogger@br-automation.com>
2012-08-03 16:33:15 +0200

    [FIX] use existing define names for Async TX buffer sizes
     * this is a fix for commit:
       [FEATURE] Add EplCfg parameters for Async TX buffer size

--- powerlink/pcp_DirectIO/EplCfg.h
+++ powerlink/pcp_DirectIO/EplCfg.h
@@ -204,11 +204,11 @@
 #define EPL_DLL_DISABLE_DEFERRED_RXFRAME_RELEASE    TRUE
 
 // Async buffer for NMT commands TX in bytes(IdentResponse, StatusResponse)
-#define EPL_DLLCAL_TX_BUFFER_SIZE_NMT   4096
+#define EPL_DLLCAL_BUFFER_SIZE_TX_NMT   4096
 // Async buffer for Asnd messages TX in bytes
-#define EPL_DLLCAL_TX_BUFFER_SIZE_GEN   8192
+#define EPL_DLLCAL_BUFFER_SIZE_TX_GEN   8192
 // Async buffer for Sync Response TX in bytes
-#define EPL_DLLCAL_TX_BUFFER_SIZE_SYNC  4096
+#define EPL_DLLCAL_BUFFER_SIZE_TX_SYNC  4096
 
 // =========================================================================
 // OBD specific defines
--- powerlink/pcp_PDI/EplCfg.h
+++ powerlink/pcp_PDI/EplCfg.h
@@ -206,11 +206,11 @@
 #define EPL_PDOK_ENDIAN_CONVERSION      FALSE
 
 // Async buffer for NMT commands TX in bytes(IdentResponse, StatusResponse)
-#define EPL_DLLCAL_TX_BUFFER_SIZE_NMT   4096
+#define EPL_DLLCAL_BUFFER_SIZE_TX_NMT   4096
 // Async buffer for Asnd messages TX in bytes
-#define EPL_DLLCAL_TX_BUFFER_SIZE_GEN   8192
+#define EPL_DLLCAL_BUFFER_SIZE_TX_GEN   8192
 // Async buffer for Sync Response TX in bytes
-#define EPL_DLLCAL_TX_BUFFER_SIZE_SYNC  4096
+#define EPL_DLLCAL_BUFFER_SIZE_TX_SYNC  4096
 
 
 // =========================================================================
