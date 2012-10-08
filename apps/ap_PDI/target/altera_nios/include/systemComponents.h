a18e7ecf546cc5c06eb3d13e15f276ced5165a64
Thomas Mair <thomas.mair@br-automation.com>
2012-10-08 14:40:37 +0200

    [TASK] Change AP headers and formating

--- apps/ap_PDI/target/altera_nios/include/systemComponents.h
+++ apps/ap_PDI/target/altera_nios/include/systemComponents.h
@@ -1,44 +1,15 @@
 /**
 ********************************************************************************
-\file           systemComponents.h
+\file       systemComponents.h
 
-\brief          Header file which contains processor specific definitions
-                (altera version)
+\brief      Header file which contains processor specific definitions
+            (Nios II version)
 
-This header file contains processor specific definitions.
+This header file contains of platform specific definitions.
 
-********************************************************************************
-
-License Agreement
-
-Copyright (C) 2011 BERNECKER + RAINER, AUSTRIA, 5142 EGGELSBERG, B&R STRASSE 1
-All rights reserved.
-
-Redistribution and use in source and binary forms,
-with or without modification,
-are permitted provided that the following conditions are met:
-
-  * Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer
-    in the documentation and/or other materials provided with the
-    distribution.
-  * Neither the name of the B&R nor the names of its contributors
-    may be used to endorse or promote products derived from this software
-    without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Copyright © 2011 BERNECKER + RAINER, AUSTRIA, 5142 EGGELSBERG, B&R STRASSE 1
+All rights reserved. All use of this software and documentation is
+subject to the License Agreement located at the end of this file below.
 
 *******************************************************************************/
 
@@ -132,6 +103,8 @@ int SysComp_SPICommand(unsigned char *pTxBuf_p, unsigned char *pRxBuf_p, int iBy
 void SysComp_writeOutputPort(DWORD dwValue_p);
 DWORD SysComp_readInputPort();
 
+#endif /* SYSTEMCOMPONENTS_H_ */
+
 /*******************************************************************************
 *
 * License Agreement
@@ -166,5 +139,4 @@ DWORD SysComp_readInputPort();
 * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *******************************************************************************/
-#endif /* SYSTEMCOMPONENTS_H_ */
 /* END-OF-FILE */
--- apps/ap_PDI/target/xilinx_microblaze/src/systemComponents.c
+++ apps/ap_PDI/target/xilinx_microblaze/src/systemComponents.c
@@ -1,24 +1,19 @@
-/*******************************************************************************
-* Copyright © 2011 BERNECKER + RAINER, AUSTRIA, 5142 EGGELSBERG, B&R STRASSE 1
-* All rights reserved. All use of this software and documentation is
-* subject to the License Agreement located at the end of this file below.
-*/
-
 /**
 ********************************************************************************
-
 \file       systemComponents.c
 
-\brief      Module which contains of processor specific functions
-            (microblaze version)
+\brief      Header file which contains processor specific definitions
+            (Microblaze version)
 
-\author     mairt
+This module contains of platform specific definitions.
 
-\date       14.02.2012
-
-\since      14.02.2012
+Copyright © 2011 BERNECKER + RAINER, AUSTRIA, 5142 EGGELSBERG, B&R STRASSE 1
+All rights reserved. All use of this software and documentation is
+subject to the License Agreement located at the end of this file below.
 
 *******************************************************************************/
+
+/*******************************************************************************/
 /* includes */
 #include "systemComponents.h"
 
@@ -310,4 +305,40 @@ DWORD SysComp_readInputPort(void)
 
     return dwValue;
 }
+
+/*******************************************************************************
+*
+* License Agreement
+*
+* Copyright © 2011 BERNECKER + RAINER, AUSTRIA, 5142 EGGELSBERG, B&R STRASSE 1
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms,
+* with or without modification,
+* are permitted provided that the following conditions are met:
+*
+*   * Redistributions of source code must retain the above copyright notice,
+*     this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above copyright notice,
+*     this list of conditions and the following disclaimer
+*     in the documentation and/or other materials provided with the
+*     distribution.
+*   * Neither the name of the B&R nor the names of its contributors
+*     may be used to endorse or promote products derived from this software
+*     without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+* A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+* THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*******************************************************************************/
+/* END-OF-FILE */
 
